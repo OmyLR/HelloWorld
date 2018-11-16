418e5c7309441186a1c9543613acb95815fa04fa
OmyLR <omysam@gmail.com>
2018-11-16 09:23:40 +0000

    Initial commit

---
+++ src/helloworld/com/web/service/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld.com.web.service;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {}
+	
+	public float addValue(float value) {
+		return (value + 10);
+	}
+ 
+	public float subtractValue(float value) {
+		return (value - 10);
+	}
+	
+}
